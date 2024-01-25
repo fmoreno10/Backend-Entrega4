fd0b8784a619f6f6c29a9c094f036c9150caf768
Facundo Moreno <fdam10@yahoo.com.ar>
2024-01-25 00:31:29 -0300

    Entrega 4 v2

--- src/app.js
+++ src/app.js
@@ -6,11 +6,25 @@ import { productsRouter } from './routes/products.router.js';
 import { cartsRouter } from './routes/carts.router.js';
 
 import { Server } from 'socket.io';
+
 const app = express();
-const httpServer = 
+const httpServer = app.listen(8080, () => console.log('Escuchando en puerto 8080...'));
+
+const socketServer = new Server(httpServer);
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engines', 'handlebars');
+app.use(express.static(__dirname + 'public'));
 
 app.use(express.urlencoded({ extended: true })) // Para usar forms
 app.use(express.json())
+
+app.use('/', viewsRouter);
+
+socketServer.on('connection', socket => console.log('Nuevo cliente conectado...'));
+
+socketServer.on('message', data => console.log(data));
 app.use('/api/', productsRouter);
 app.use('/api/', cartsRouter);
 
--- src/public/index.js
+++ src/public/index.js
@@ -1,2 +1,10 @@
 // instanciamos el socket desde el cliente, para comunicarse con el servidor
 const socket = io();
+
+socket.emit('message', 'Hola me estoy comunicando por websocket');
+
+socket.on('evento_para_socket_individual', data => console.log(data));
+
+socket.on('evento_para_todos_los_socket_menos_este', data => console.log(data));
+
+socket.on('evento_para_todos', data => console.log(data));
